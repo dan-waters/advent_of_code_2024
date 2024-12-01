b6992c1bf97abab261e55d411e6bef9145150423
Daniel Waters <daniel@softwire.com>
2024-12-01 22:36:34 +0000

    day 1 of 2024

---
+++ day1/main.rb
@@ -0,0 +1,33 @@
+require 'open-uri'
+
+list1= []
+
+list2 = []
+
+open(__dir__ + '/input.txt').readlines(chomp: true).each do |line|
+  x, y = line.split(/ +/)
+  list1 << x.to_i
+  list2 << y.to_i
+end
+
+list1.sort!
+list2.sort!
+
+# Part 1
+
+total = list1.zip(list2).reduce(0) do |total, pair|
+  x, y = pair
+  total + (x-y).abs
+end
+
+puts total
+
+# Part 2
+
+similarity_score = 0
+
+list1.each do |x|
+  similarity_score += x*list2.count(x)
+end
+
+puts similarity_score
